c3c308fc5e4ba6380deb0b24d1d3421f268cf8da
AmirolAhmad <dev.amirol@gmail.com>
2019-05-10 11:46:51 +0800

    update the latest bootstrap version

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React Redux Boilerplate</div>
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Holy guacamole!</strong> React Redux Boilerplate.
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
     );
   }
 }
